b62cfb15246972555706bfedce4aa9c2d3a08fba
Gasijiyo <pys9307@naver.com>
2023-07-26 21:18:59 +0900

    .

--- Understanding-TS/app.ts
+++ Understanding-TS/app.ts
@@ -1,6 +1,17 @@
+// 기본 값 설정 시 맨 앞인자 말고 뒤 인자부터 설정해야 함.
+const add1 = (a: number , b:number=1) => a+b;
+// const add1 = (a: number =1, b:number) => a+b;
+const printOutput : (a: number|string) => void = output => console.log(output);
+
 const button = document.querySelector('button')!;
 // '!'는 기본적으로 값이 존재하니 값이 반환될거라고 TS에 알려주는 것
-button.addEventListener('click', () => {
-    console.log('Clicked!');
-});
+// button.addEventListener('click', () => {
+//     console.log('Clicked!');
+// });
+
+if(button){
+    button.addEventListener('click', event => console.log(event)); 
+}
+
+printOutput(add1(5));
 
--- Understanding-TS/basics.ts
+++ Understanding-TS/basics.ts
@@ -1,4 +1,4 @@
-function add(n1, n2: number, showResult: boolean, phrase: string){
+function add2(n1: any, n2: number, showResult: boolean, phrase: string){
     // if(typeof n1 === 'number' && typeof n2 === 'number'){
     //     throw new Error('Incorrect Input!');
     // }
@@ -13,8 +13,8 @@ function add(n1, n2: number, showResult: boolean, phrase: string){
 let number1: number;
 number1 = 5;
 const number2 = 2.8;
-const printResult = true;
+const printResult1 = true;
 let resultPhrase = 'Result is: ';
 
 
-add(number1, number2, printResult, resultPhrase);
+add2(number1, number2, printResult1, resultPhrase);
