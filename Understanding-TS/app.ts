9de69ffba283ad34794e831e7ceebfab64537174
Gasijiyo <pys9307@naver.com>
2023-07-30 08:27:52 +0900

    .

--- Understanding-TS/app.ts
+++ Understanding-TS/app.ts
@@ -15,3 +15,27 @@ if(button){
 
 printOutput(add1(5));
 
+const hobbies = ['Sports','Cooking'];
+const activeHobbies = ['Hiking',...hobbies];
+
+/** ... : 전개연산자
+ *  전개하고자 하는 배열 또는 객체를 지정 또는 지시
+ *  해당 배열 또는 객체의 모든 요소를 꺼내 값의 목록으로 추가
+ *  
+ */
+var resultDot = activeHobbies.push(...hobbies);
+console.log("result ... : "+resultDot);
+console.log(activeHobbies);
+
+activeHobbies.splice(2,0,'Working');
+// splice: (배열index, 삭제할 값 개수, 삭제한 배열에 인덱스에 추가할 값)
+// activeHobbies.splice(2,1,'Working');
+console.log(activeHobbies);
+
+const aPerson = {
+    name: 'Bill',
+    age: 29,
+};
+
+const copiedAPerson = {...aPerson}; 
+// = aPerson 이라고 하면 pointer값만 가져오는 것임 (메모리 주소값)
